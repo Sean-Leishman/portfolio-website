4c36f1ba04a1e49bbebf202d68c6fe996a86ca20
Sean-Leishman <leishmansean@gmail.com>
2022-09-30 14:44:26 +0800

    Miscellaneous changes

--- src/client/index.js
+++ src/client/index.js
@@ -14,16 +14,3 @@ import './init_models.js';
 
 import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, getDoc } from 'firebase/firestore/lite';
-/*
-const firebaseConfig = {
-    apiKey: "AIzaSyAKpwy-KRcA-u8loE638kmip_qiaWRZItA",
-    authDomain: "portolfio-2c85b.firebaseapp.com",
-    projectId: "portolfio-2c85b",
-    storageBucket: "portolfio-2c85b.appspot.com",
-    messagingSenderId: "433920767655",
-    appId: "1:433920767655:web:b6b3b842901765ab45cd82",
-    measurementId: "G-M200SV9SNS"
-  };
-const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
-*/
