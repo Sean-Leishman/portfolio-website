6a29d8ff7e6a629d1ac175525c7c4469f9133b16
Sean-Leishman <leishmansean@gmail.com>
2022-10-19 16:12:33 +0800

    Added links and assets

---
+++ webpack.common.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssWebpackPlugin = require('mini-css-extract-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/client/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/client/index.html',
+        filename: 'index.html'
+    }),
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+    })
+],
+output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+},
+module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader','css-loader'],
+        },
+        {
+            test: /\.hdr$/i,
+            use: [
+                {
+                    loader: 'url-loader',
+                    options:
+                    {
+                        outputPath: 'assets/static/'
+                    }
+                }
+            ],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            //type: 'asset/resource',
+            use: 
+            [   
+                {
+                    loader: 'url-loader',
+                    options:
+                    {
+                        name: '[path][name].[ext]',
+                        outputPath: 'assets/images/'
+                    }
+                }
+            ]
+        },
+        {
+            test: /\.(glb|gltf)$/,
+            use:
+            [
+                {
+                    loader: 'file-loader',
+                    options:
+                    {
+                        name: '[path][name].[ext]',
+                        outputPath: 'assets/models/'
+                    }
+                }
+            ]
+        },
+    ]
+}
+};
